9a622bb2d29c95c7f34dde5c60a363d1e68e1028
Adam Allen <allena29@users.noreply.github.com>
2019-05-12 16:56:30 +0100

    Update tox config

--- setup.py
+++ setup.py
@@ -4,9 +4,8 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 os.chdir('clients')
-
 setuptools.setup(name='yangvoodoo',
-                 version='0.0.77', author='Adam Allen',
+                 version='0.0.3', author='Adam Allen',
                  author_email='allena29@users.noreply.github.com',
                  description='Python based access to YANG Datatstores',
                  long_description=long_description,
@@ -17,6 +16,9 @@ setuptools.setup(name='yangvoodoo',
                      'libyang',
                      'cffi',
                  ],
+                 zip_safe=False,
+                 include_package_data=True,
+
                  classifiers=['Programming Language :: Python :: 3',
                               'License :: OSI Approved :: Apache License 2.0',
                               'Operating System :: OS Independent'])
