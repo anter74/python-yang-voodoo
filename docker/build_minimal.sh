edde8aa9460ccada44761c4caf3ed6111d4321bb
Adam Allen <allena29@users.noreply.github.com>
2019-08-11 17:09:43 +0100

    Dockerfiles for a minimal sysrepo-less image.

---
+++ docker/build_minimal.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# This version is minimal
+
+
+set pipefail -euo
+
+echo "Building image and compiling everything...."
+img=`docker build builder | tail -n 1 | sed -e's/Successfully built //'`
+docker tag $img allena29/yangvoodoo:builder
+
+container=`docker run -i -d $img /bin/bash`
+
+echo "Image $img, Container $container"
+if [ -d devel/artefacts ]
+then
+  rm -fr devel/artefacts
+fi
+
+echo "Copying deb pacakge"
+
+docker cp $container:/artefacts minimal
+wget https://bootstrap.pypa.io/get-pip.py -O minimal/artefacts/get-pip.py
+git clone -b master https://github.com/allena29/python-yang-voodoo.git minimal/artefacts/working
+docker stop $container
+
+
+rm -fr minimal/artefacts/*.tar
+
+img=`docker build minimal | tail -n 1 | sed -e's/Successfully built //'`
+echo "Built minimal development image"
+docker tag $img allena29/yangvoodoo:libyang-only
