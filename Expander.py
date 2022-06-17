d3971bd0d71e0cfeaf06a3fc462cc4605dc3051d
Adam Allen <allena29@users.noreply.github.com>
2022-06-17 16:48:04 +0100

    Enrich information in the merged data/schema payload

--- Expander.py
+++ Expander.py
@@ -8,6 +8,7 @@ class SchemaDataExpander:
 
     SCHEMA_NODE_TYPE_MAP = {1: "_handle_schema_containing_node", 4: "_handle_schema_leaf", 16: "_handle_schema_list"}
     DATA_NODE_TYPE_MAP = {1: "_handle_data_containing_node", 4: "_handle_data_leaf"}
+    LEAF_TYPE_MAP = {10: "string", 6: "enum", 3: "boolean", 13: "uint8"}
 
     def __init__(self, yang_module, log):
         self.log = log
@@ -18,10 +19,8 @@ class SchemaDataExpander:
 
     def process_schema(self):
         """
-        Return just schema nodes exapnded out?
-        and the post-process data into it?
+        Return every node from the schema
         """
-
         self.log.info("%s processing schema - %s", __name__, self.yang_module)
         self.log.debug("libyang context %s", self.ctx)
 
@@ -42,15 +41,12 @@ class SchemaDataExpander:
             yield from getattr(self, self.SCHEMA_NODE_TYPE_MAP[node.nodetype()])(node)
 
     def _handle_schema_leaf(self, node):
-        yield SchemaDataExpander._form_structure(node, "leaf")
-        self.log.warning("TODO: add in enumeration so we can present valid options")
-        self.log.warning("TODO: add in boolean/emptyleaf so we can present a checkbox")
-        self.log.warning("TODO: add in union - future?")
-        self.log.warning("TODO: add in custom types so we can do regex validation?")
+        yield SchemaDataExpander._add_leaf_type(node, SchemaDataExpander._form_structure(node, "leaf"))
 
     def _handle_schema_list(self, node):
-        yield SchemaDataExpander._form_structure(node, "list")
-        self.log.warning("TODO: add in list keys - so we know mandatory minimum information")
+        yield SchemaDataExpander._add_list_key_definition(
+            node, SchemaDataExpander._form_structure(node, "list-element")
+        )
 
         for node in self.ctx.find_path(f"{node.schema_path()}/*"):
             if node.nodetype() not in self.SCHEMA_NODE_TYPE_MAP:
@@ -59,11 +55,29 @@ class SchemaDataExpander:
 
     @staticmethod
     def _form_structure(node, nodetype):
-        return {
-            "schema": node.schema_path(),
-            "type": nodetype,
-            "data": {},
-        }
+        return {"schema": node.schema_path(), "type": nodetype, "data": {}, "validations": []}
+
+    @classmethod
+    def _add_leaf_type(cls, node, structure):
+        leaftype = node.type().base()
+        if leaftype not in cls.LEAF_TYPE_MAP:
+            raise NotImplementedError(f"{node.schema_path()} has unknown leaf type {leaftype}")
+        leaftype = cls.LEAF_TYPE_MAP[node.type().base()]
+        structure["subtype"] = leaftype
+
+        if leaftype == "enum":
+            structure["validations"].append({"explicit": [enum[0] for enum in node.type().enums()]})
+
+        if leaftype == "string":
+            patterns = [pattern[0] for pattern in node.type().all_patterns()]
+            if patterns:
+                structure["validations"].append({"regex": patterns})
+        return structure
+
+    @staticmethod
+    def _add_list_key_definition(node, structure):
+        structure["keys"] = [key.name() for key in node.keys()]
+        return structure
 
     @staticmethod
     def _get_parent(node):
